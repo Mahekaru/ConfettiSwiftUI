fabc78829d1b0493978c9558ded2766d0ba6657d
Mahekaru <Mahekaru@gmail.com>
2022-07-26 14:56:59 -0400

    Adding shooting conffetti

--- Tests/ConfettiSwiftUITests/ConfettiSwiftUITests.swift
+++ Tests/ConfettiSwiftUITests/ConfettiSwiftUITests.swift
@@ -7,7 +7,7 @@ final class ConfettiSwiftUITests: XCTestCase {
     @State var counter = 0
     
     func testExample() {
-        ConfettiSwiftUI.ConfettiCannon(counter:$counter)
+        ConfettiSwiftUI.ConfettiCannon(counter:$counter).shootConfetti()
         Button("Animation"){
             self.counter += 1
         }
